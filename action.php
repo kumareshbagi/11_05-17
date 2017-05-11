77b74f3f539c18db81a36f667dcf7184e295f7b8
kumareshbagi <kumaresh.517@gmail.com>
2017-05-11 17:25:47 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,21 @@
+<?php
+echo"Hello";
+?>
+<?php
+echo"Your details are";?><br>
+<?php
+echo"NAme:";
+echo $_POST['name'];?><br>
+<?php
+echo"Place:";
+echo $_POST['place'];
+?><br>
+<?php
+echo"Dist";
+echo $_POST['dist'];
+?><br>
+<?php
+echo"Hobbies:";
+echo $_POST['hobbies'];
+?>
+
